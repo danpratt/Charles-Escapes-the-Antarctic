d74674857a1143f1789566fd474f9936b9320fe6
Daniel Pratt <daniel@blaumagier.com>
2018-01-18 14:49:01 -0800

    feat: demo playground with some nodes
    - added a square node
    - set background color to sky blue
    - setup some actions and running them as a group

--- Charles Escapes the Antarctic/GameScene.swift
+++ Charles Escapes the Antarctic/GameScene.swift
@@ -15,5 +15,25 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
+        // Set background to sky blue
+        backgroundColor = UIColor(red: 0.4, green: 0.6, blue: 0.95, alpha: 1.0)
+        
+        // create a constant sprite node
+        let mySprite = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 50))
+        
+        // set position of mySprite
+        let position = CGPoint(x: view.bounds.width * 0.75, y: view.bounds.height * 0.666)
+        mySprite.position = position
+        
+        // add sprite to node tree
+        addChild(mySprite)
+        
+        // setup action for sprite
+        let demoAction = SKAction.move(to: CGPoint(x: view.bounds.width * 0.25 / 2, y: view.bounds.height * 0.666), duration: 5)
+        let demoResizeAction = SKAction.scale(to: 2, duration: 5)
+        let demoRotateAction = SKAction.rotate(byAngle: 4 * CGFloat.pi, duration: 5)
+        let actionGroup = SKAction.group([demoResizeAction, demoAction, demoRotateAction])
+        mySprite.run(SKAction.sequence([actionGroup]))
+        
     }
 }
