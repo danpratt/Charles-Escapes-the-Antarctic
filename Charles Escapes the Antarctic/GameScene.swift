39d25d5e7d9e9e623a9cd27bf8a8470711ae892d
Daniel Pratt <daniel@blaumagier.com>
2018-01-18 15:28:51 -0800

    feat: added bee images atlas and animated bee

--- Charles Escapes the Antarctic/GameScene.swift
+++ Charles Escapes the Antarctic/GameScene.swift
@@ -19,15 +19,28 @@ class GameScene: SKScene {
         backgroundColor = UIColor(red: 0.4, green: 0.6, blue: 0.95, alpha: 1.0)
         
         // create a bee
-        let bee = SKSpriteNode(imageNamed: "bee")
+        let bee = SKSpriteNode()
         
-        // set the size
-        bee.size = CGSize(width: 100, height: 100)
+        // set bee size
+        bee.size = CGSize(width: 28, height: 24)
         
         // set position
         bee.position = CGPoint(x: view.bounds.width / 2, y: view.bounds.height * 0.75)
         
         // add bee to scene
         addChild(bee)
+        
+        // setup bee atlas for animation
+        let beeAtlas = SKTextureAtlas(named: "bee")
+        let beeFrames: [SKTexture] = [
+            beeAtlas.textureNamed("bee"),
+            beeAtlas.textureNamed("bee-fly")
+        ]
+        
+        // frames to repeat
+        let flyAction = SKAction.animate(with: beeFrames, timePerFrame: 0.14)
+        // animate forever
+        let beeFlyAction = SKAction.repeatForever(flyAction)
+        bee.run(beeFlyAction)
     }
 }
